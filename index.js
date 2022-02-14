dd65059288d18fe3a87f1f1356c9268e01d822cf
Sven Lüchow <sven.luechow@icloud.com>
2022-02-14 20:21:40 +0100

    Add function to set bookmarked in first Card

---
+++ index.js
@@ -0,0 +1,7 @@
+//Variable zum Ändern der Hintergrundfarbe
+const buttonToggle = document.querySelector("[data-js=toggleBookmark]");
+
+//Ändere die Hintergrundfarbe von den Card-Bookmarks
+buttonToggle.addEventListener("click", () => {
+  buttonToggle.classList.toggle("card__bookmark--set");
+});
