93f8466436ad63f3763acc35e561c5b90c50950c
Sven Lüchow <sven.luechow@icloud.com>
2022-02-15 19:05:24 +0100

    Add JS to all Bookmarks on index.html

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-//Variable zum Ändern der Hintergrundfarbe
+//1. QuestionCard
+//Variable zum Ändern der Hintergrundfarbe im Bookmark
 const buttonToggle = document.querySelector('[data-js=toggleBookmark]');
 
 //Ändere die Hintergrundfarbe von den Card-Bookmarks
 buttonToggle.addEventListener('click', () => {
   buttonToggle.classList.toggle('card__bookmark--set');
+});
+
+//1. AnswerCard
+const buttonToggle1 = document.querySelector('[data-js=toggleBookmark1]');
+buttonToggle1.addEventListener('click', () => {
+  buttonToggle1.classList.toggle('card__bookmark--set');
+});
+
+//2. QuestionCard
+const buttonToggle2 = document.querySelector('[data-js=toggleBookmark2]');
+buttonToggle2.addEventListener('click', () => {
+  buttonToggle2.classList.toggle('card__bookmark--set');
+});
+
+//2. AnswerCard
+const buttonToggle3 = document.querySelector('[data-js=toggleBookmark3]');
+buttonToggle3.addEventListener('click', () => {
+  buttonToggle3.classList.toggle('card__bookmark--set');
+});
+
+//3. QuestionCard
+const buttonToggle4 = document.querySelector('[data-js=toggleBookmark4]');
+buttonToggle4.addEventListener('click', () => {
+  buttonToggle4.classList.toggle('card__bookmark--set');
+});
+
+//3. AnswerCard
+const buttonToggle5 = document.querySelector('[data-js=toggleBookmark5]');
+buttonToggle5.addEventListener('click', () => {
+  buttonToggle5.classList.toggle('card__bookmark--set');
+});
+
+//4. QuestionCard
+const buttonToggle6 = document.querySelector('[data-js=toggleBookmark6]');
+buttonToggle6.addEventListener('click', () => {
+  buttonToggle6.classList.toggle('card__bookmark--set');
+});
+
+//4. AnswerCard
+const buttonToggle7 = document.querySelector('[data-js=toggleBookmark7]');
+buttonToggle7.addEventListener('click', () => {
+  buttonToggle7.classList.toggle('card__bookmark--set');
 });
