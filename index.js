26e8533e759658f7fd4115167615292062b34cb3
Sven Lüchow <sven.luechow@icloud.com>
2022-02-15 20:56:04 +0100

    Add hide toggle on Answers at index.html

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-// ### 1. QuestionCard ###
-//Variable zum Ändern der Hintergrundfarbe im Bookmark
-const buttonToggle = document.querySelector('[data-js=toggleBookmark]');
-//Ändere die Hintergrundfarbe von den Card-Bookmarks
-buttonToggle.addEventListener('click', () => {
-  buttonToggle.classList.toggle('card__bookmark--set');
-});
-//Ändere die Hintergrundfarbe und Border-Color von Show-Answer
-const buttonShowAnswer = document.querySelector('[data-js=showAnswer]');
-buttonShowAnswer.addEventListener('click', () => {
-  buttonShowAnswer.classList.toggle('card__button-hide-answer');
-});
-
 //1. AnswerCard
 const buttonToggle1 = document.querySelector('[data-js=toggleBookmark1]');
 buttonToggle1.addEventListener('click', () => {
   buttonToggle1.classList.toggle('card__bookmark--set');
 });
 const buttonHideAnswer = document.querySelector('[data-js=hideAnswer]');
+const showAnswerOne = document.querySelector('[data-js="answerOne"]');
 buttonHideAnswer.addEventListener('click', () => {
   buttonHideAnswer.classList.toggle('card__button-hide-answer');
-});
-
-//2. QuestionCard
-const buttonToggle2 = document.querySelector('[data-js=toggleBookmark2]');
-buttonToggle2.addEventListener('click', () => {
-  buttonToggle2.classList.toggle('card__bookmark--set');
-});
-const buttonShowAnswer1 = document.querySelector('[data-js=showAnswer1]');
-buttonShowAnswer1.addEventListener('click', () => {
-  buttonShowAnswer1.classList.toggle('card__button-hide-answer');
+  showAnswerOne.toggleAttribute('hidden');
 });
 
 //2. AnswerCard
@@ -37,18 +16,10 @@ buttonToggle3.addEventListener('click', () => {
   buttonToggle3.classList.toggle('card__bookmark--set');
 });
 const buttonHideAnswer1 = document.querySelector('[data-js=hideAnswer1]');
+const showAnswerTwo = document.querySelector('[data-js="answerTwo"]');
 buttonHideAnswer1.addEventListener('click', () => {
   buttonHideAnswer1.classList.toggle('card__button-hide-answer');
-});
-
-//3. QuestionCard
-const buttonToggle4 = document.querySelector('[data-js=toggleBookmark4]');
-buttonToggle4.addEventListener('click', () => {
-  buttonToggle4.classList.toggle('card__bookmark--set');
-});
-const buttonShowAnswer2 = document.querySelector('[data-js=showAnswer2]');
-buttonShowAnswer2.addEventListener('click', () => {
-  buttonShowAnswer2.classList.toggle('card__button-hide-answer');
+  showAnswerTwo.toggleAttribute('hidden');
 });
 
 //3. AnswerCard
@@ -57,18 +28,10 @@ buttonToggle5.addEventListener('click', () => {
   buttonToggle5.classList.toggle('card__bookmark--set');
 });
 const buttonHideAnswer2 = document.querySelector('[data-js=hideAnswer2]');
+const showAnswerThree = document.querySelector('[data-js="answerThree"]');
 buttonHideAnswer2.addEventListener('click', () => {
   buttonHideAnswer2.classList.toggle('card__button-hide-answer');
-});
-
-//4. QuestionCard
-const buttonToggle6 = document.querySelector('[data-js=toggleBookmark6]');
-buttonToggle6.addEventListener('click', () => {
-  buttonToggle6.classList.toggle('card__bookmark--set');
-});
-const buttonShowAnswer3 = document.querySelector('[data-js=showAnswer3]');
-buttonShowAnswer3.addEventListener('click', () => {
-  buttonShowAnswer3.classList.toggle('card__button-hide-answer');
+  showAnswerThree.toggleAttribute('hidden');
 });
 
 //4. AnswerCard
@@ -77,6 +40,8 @@ buttonToggle7.addEventListener('click', () => {
   buttonToggle7.classList.toggle('card__bookmark--set');
 });
 const buttonHideAnswer3 = document.querySelector('[data-js=hideAnswer3]');
+const showAnswerFour = document.querySelector('[data-js="answerFour"]');
 buttonHideAnswer3.addEventListener('click', () => {
   buttonHideAnswer3.classList.toggle('card__button-hide-answer');
+  showAnswerFour.toggleAttribute('hidden');
 });
