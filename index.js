3447722e6edad796b8431ced6bedd1a496125147
Sven Lüchow <sven.luechow@icloud.com>
2022-02-18 17:11:38 +0100

    Refactor cards functionality

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 import navigation from './JS/navigation.js';
+import cards from './JS/cards.js';
 
 navigation();
-
-// Set Bookmark
-const bookmarkButtons = document.querySelectorAll('[data-js=toggleBookmark]');
-
-bookmarkButtons.forEach(bookmarkButton => {
-  bookmarkButton.addEventListener('click', () => {
-    bookmarkButton.classList.toggle('card__bookmark--set');
-  });
-});
-// Show-Hide-Answer-Button
-const showAnswerButtons = document.querySelectorAll(
-  '[data-js=showAnswerButton]'
-);
-const showAnswerText = document.querySelectorAll('[data-js="answerText"]');
-let isAnswerShown = true;
-
-showAnswerButtons.forEach(showAnswerButton => {
-  showAnswerButton.addEventListener('click', () => {
-    showAnswerText.toggleAttribute('hidden');
-    if (isAnswerShown) {
-      showAnswerButton.textContent = 'Hide Answer';
-      isAnswerShown = false;
-    } else {
-      showAnswerButton.textContent = 'Show Answer';
-      isAnswerShown = true;
-    }
-  });
-});
+cards();
